ff244aeafde1dba8173c8aeb9f0fe56c3af551b5
erikisaac <erik@erikisaac.com>
2016-12-23 14:25:34 -0700

    Angular online.

--- public/main.js
+++ public/main.js
@@ -1,1 +1,12 @@
-console.log("main.js online!")
+console.log("main.js online!");
+
+var app = angular.module("mean_crudApp", []);
+
+app.controller("HelloWorldController", HelloWorldController);
+
+function HelloWorldController(){
+  this.helloWorld = function(hi) {
+		hi = "Hello world!";
+		return hi;
+	};
+};
