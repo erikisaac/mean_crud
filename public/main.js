24b8d602463bd35942b83d5c757e120d598c6721
erikisaac <erik@erikisaac.com>
2017-01-02 09:44:59 -0700

    First commit after break. I'm in the first steps of setting up the coniction to the db. index.html and main.js has the code.

--- public/main.js
+++ public/main.js
@@ -4,8 +4,23 @@ var app = angular.module("mean_crudApp", []);
 
 app.controller("HelloWorldController", HelloWorldController);
 
-function HelloWorldController(){
-  this.helloWorld = function(hi) {
+function HelloWorldController($http){
+	var self = this;
+	self.all = [];
+
+	self.getNames = getNames;
+
+	function getNames(){
+		$http
+			.get("http://localhost:3000/names")
+			.then(function(response) {
+				self.all = resopnse.data.name;
+			});
+	};
+
+	getNames();
+
+	this.helloWorld = function(hi) {
 		hi = "Hello world!";
 		return hi;
 	};
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+// Load mongoose package
+var mongoose = require('mongoose');
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/mean_crud');
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is running on http://localhost:3000/');
 });
